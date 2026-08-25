f07a93c91ace55cea5dd5eda4c12f6ecf82b8dbd
JatziGonzalez <dan7kot@gmail.com>
2026-08-25 19:54:40 +0000

    qwerty

---
+++ worldcup.py
@@ -0,0 +1,15 @@
+def main():
+    spain = input("Spain goals: ")
+    argentina = input("Argentina goals: ")
+
+    if spain > argentina:
+        print("Spain is the Winner!")
+    elif argentina > spain:
+        print("Argentina is the Winner!")
+    else:
+        print("It's a tie.")
+
+    print("gg")
+    
+if__name__=="__main__":
+    main()
