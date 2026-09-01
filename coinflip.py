a2e8e0279674c4465344defdc5511495e8a59a08
JatziGonzalez <dan7kot@gmail.com>
2026-09-01 20:18:20 +0000

    i hate and love coding

---
+++ coinflip.py
@@ -0,0 +1,21 @@
+import random
+def main():
+
+    guess = int(input("If you guess TAILS enter 1, If you guess HEADS enter 2: "))
+
+    number = random.randint(1,2)
+
+    if number == 1:
+        print ("Tails")
+    else:
+        print("Heads")
+
+    if guess == number:
+        print ("Winner")
+    else:
+        print ("Loser")
+
+
+
+if __name__ =="__main__":
+    main()
