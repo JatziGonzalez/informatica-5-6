c56110669d2f8659e40f508fe79243bbece2888d
JatziGonzalez <dan7kot@gmail.com>
2026-09-02 17:53:37 +0000

    letssgooo

---
+++ roadtrip.py
@@ -0,0 +1,9 @@
+def main():
+    answer = ""
+
+    while answer != "Yes":
+        answer = input("Are we there yet? ").title().strip()
+    print("We are here!")
+
+if __name__ =="__main__":
+    main()
