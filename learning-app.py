026423caeba75db860e3155d82ff5acb0233fc64
JatziGonzalez <dan7kot@gmail.com>
2026-09-09 17:41:20 +0000

    finishhshd

--- learning-app.py
+++ learning-app.py
@@ -2,31 +2,28 @@ import random
 def main():
 
     print("Get SmarTer")
+    streak = 0
 
-    number1 = random.randint(10,99)
-    number2 = random.randint(10,99)
-    result = int((number1 + number2))
- 
+    while streak < 3:
 
-    response = int((input(f"{number1}+{number2}=")))
-
-
-    while response < 3:
-        print("")
-
-    if response == result:
-        print("Correct Result ⭐")
-        response += 1
-    else:
-        print("Try again")
+        number1 = random.randint(10,99)
+        number2 = random.randint(10,99)
+        result = int((number1 + number2))
+        response = int((input(f"{number1}+{number2}=")))
 
 
+        if response == result:
+            print("Correct Result")
+            streak += 1
+            print("Streak ⭐")
+        else:
+            print("Try again")
+            print ("The answer was", result)
 
+            streak = 0
+            print("")
 
-
-
-
-
+    print("You got 3 correct ⭐⭐⭐")
 
 
 if __name__ =="__main__":
